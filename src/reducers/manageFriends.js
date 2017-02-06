adb80552834bd4697a3d87fec500cd2902671240
jkhoang313 <jkhoang313@gmail.com>
2017-02-06 16:21:01 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+  if (action.type === "ADD_FRIEND") {
+    return Object.assign({}, state, {friends: state.friends.concat(action.payload)})
+  } else if (action.type === "REMOVE_FRIEND") {
+    var friendSelected = state.friends.findIndex( friend => friend.id === action.payload )
+    var newFriendsArray = state.friends.slice(0, friendSelected).concat(state.friends.slice(friendSelected + 1))
+    return Object.assign({}, state, {friends: newFriendsArray})
+  } else {
+    return state
+  }
 }
